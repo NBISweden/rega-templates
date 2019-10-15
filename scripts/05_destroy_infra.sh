e9a286d5877f7f0dd27390bd1bdd133e8c7ab3f6
Jon Ander Novella <jonandernovella@users.noreply.github.com>
2019-10-15 13:59:43 +0200

    Change module name in infra destroy
    Ansible module is now targeted accordingly when destroying the infra

--- scripts/05_destroy_infra.sh
+++ scripts/05_destroy_infra.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-terraform destroy -auto-approve -parallelism=10 -target=module.master -target=module.service -target=module.edge -target=module.inventory -target=module.keypair
+terraform destroy -auto-approve -parallelism=10 -target=module.master -target=module.service -target=module.edge -target=module.ansible -target=module.keypair
